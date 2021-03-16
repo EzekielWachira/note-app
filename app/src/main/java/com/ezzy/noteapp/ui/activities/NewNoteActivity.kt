da404bafa6ea91ded08d2d9ecf8086f34a4ec7fc
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-16 13:13:40 +0300

    :poop:killer code :)

--- app/src/main/java/com/ezzy/noteapp/ui/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ezzy/noteapp/ui/activities/NewNoteActivity.kt
@@ -2,10 +2,15 @@ package com.ezzy.noteapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import com.ezzy.noteapp.R
 import com.ezzy.noteapp.databinding.ActivityNewNoteBinding
+import com.ezzy.noteapp.models.Note
 
 class NewNoteActivity : AppCompatActivity() {
+
+    private val TAG = "NewNoteActivity"
 
     private lateinit var binding : ActivityNewNoteBinding
 
@@ -13,5 +18,18 @@ class NewNoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val note = Note(
+            null,
+            binding.noteTitleTextview.text.toString(),
+            binding.noteDescTextview.text.toString(),
+            "#ffffff",
+            System.currentTimeMillis()
+        )
+
+        binding.buttonAddNoteMain.setOnClickListener {
+            Log.d(TAG, "Note: $note ")
+        }
     }
 }
